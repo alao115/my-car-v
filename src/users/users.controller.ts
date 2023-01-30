380882de886d05b1f0e5bb36fac7081f0b1a28dd
alao115 <chegunmoulero@gmail.com>
2023-01-30 16:57:33 +0100

    initial push/user module and report modules setup

---
+++ src/users/users.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dtos/create-user.dto';
+
+@Controller('auth')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Post('signup')
+  createUser(@Body() body: CreateUserDto) {
+    const newUser = this.userService.create(body);
+    return newUser;
+  }
+}
