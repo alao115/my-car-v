8e67d41ba5f5da9f63d0660d1b933d8b3f95c5ff
alao115 <chegunmoulero@gmail.com>
2023-01-31 02:22:04 +0100

    User module/controller setup

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Query,
+  Delete,
+  Patch,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 
@@ -10,5 +20,25 @@ export class UsersController {
   createUser(@Body() body: CreateUserDto) {
     const newUser = this.userService.create(body);
     return newUser;
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.userService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.userService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    this.userService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.userService.update(parseInt(id), body);
   }
 }
