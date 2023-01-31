737c586a2e6e1e61c2fda9e4ee865ea991d33157
alao115 <chegunmoulero@gmail.com>
2023-01-31 16:14:14 +0100

    cookie-session module set up

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,25 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Session } from '@nestjs/common';
 import { AuthDto } from './dtos/auth.dto';
 import { AuthService } from './auth.service';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { User } from '../users/dtos/user.dto';
 
 @Controller('auth')
+@Serialize(User)
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() body: AuthDto) {
-    return this.authService.signup(body);
+  async signup(@Body() body: AuthDto, @Session() session: any) {
+    const user = await this.authService.signup(body);
+    session.userID = user.id;
+    return user;
   }
 
   @Post('signin')
-  signin(@Body() body: AuthDto) {
-    return this.authService.signin(body);
+  async signin(@Body() body: AuthDto, @Session() session: any) {
+    const user = await this.authService.signin(body);
+    session.userID = user.id;
+    return user;
   }
 }
